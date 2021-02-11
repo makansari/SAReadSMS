c0eef750678fad842249000995a9621057c07761
Admin <makansari@gmail.com>
2021-02-11 14:46:02 +0530

    Navigation Controller

--- app/src/main/java/com/example/sareadsms/Myjava.kt
+++ app/src/main/java/com/example/sareadsms/Myjava.kt
@@ -20,6 +20,15 @@ class Myjava : BroadcastReceiver() {
                     )
                     val phoneNumber = currentMessage.displayOriginatingAddress
                     val message = currentMessage.displayMessageBody
+
+                    var arr = message.split(" ").toTypedArray()
+
+                    var first = arr[0]
+                    var second = arr[1]
+
+                    Log.i("mytag","splitted $second and $first")
+
+
                     Log.i("SmsReceiver", "senderNum: $phoneNumber; message: $message")
 
 
